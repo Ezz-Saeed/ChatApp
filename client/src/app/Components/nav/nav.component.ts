d34fab98a87c99cf7288bf71cf3cd9541f6930a1
Ezz Saeed <ezzsaid299@gmail.com>
2024-12-20 05:44:51 -0800

    Fixing dropdown list of nav bar

--- client/src/app/Components/nav/nav.component.ts
+++ client/src/app/Components/nav/nav.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { AccountService } from '../../Services/account.service';
 import { CommonModule } from '@angular/common';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown'
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
@@ -31,3 +32,4 @@ export class NavComponent {
     this.isLoggesin = false;
   }
 }
+
