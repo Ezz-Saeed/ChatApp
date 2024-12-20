d082a8c07bd01cfa6914497c12f960150f1745f3
Ezz Saeed <ezzsaid299@gmail.com>
2024-12-20 04:30:50 -0800

    AccountService for client side identity functionality

--- client/src/app/Components/nav/nav.component.ts
+++ client/src/app/Components/nav/nav.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms'
+import { AccountService } from '../../Services/account.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
   model:any = {};
+  isLoggesin = false;
+
+  constructor(private accountService:AccountService){}
 
   login(){
-    console.log(this.model)
+    this.accountService.login(this.model).subscribe({
+      next:res=>{
+        console.log(res)
+        this.isLoggesin = true;
+      },
+      error:err=>{
+        console.log(err)
+      }
+    })
+  }
+
+  logout(){
+    this.isLoggesin = false;
   }
 }
