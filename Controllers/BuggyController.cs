0420caf20ab8eb9ad72836c4eed393c3470f0f10
Ezz Saeed <ezzsaid299@gmail.com>
2024-12-22 10:15:36 -0800

    Buggy controller to generate different errors

---
+++ Controllers/BuggyController.cs
@@ -0,0 +1,40 @@
+﻿using ChatApp.Data;
+using ChatApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class BuggyController(AppDbContext context) : BasApiController
+    {
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "Secret text";
+        }
+
+        [HttpGet("notFound")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = context.Users.Find(-1);
+            if(thing is null) return NotFound();
+            return Ok(thing);
+        }
+
+        [HttpGet("serverError")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = context.Users.Find(-1);
+            var thingToReturn = thing.ToString();
+            return thingToReturn;
+        }
+
+        [HttpGet("badRequest")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("Bad request!");
+        }
+
+    }
+}
